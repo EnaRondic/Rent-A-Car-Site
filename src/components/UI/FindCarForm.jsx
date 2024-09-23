bbce3dbb0d10f38a22bfa8c68aa848bca95bb303
Ena <118002989+EnaRondic@users.noreply.github.com>
2024-09-23 16:06:16 +0200

    Add Header and Footer

---
+++ src/components/UI/FindCarForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/find-car-form.css';
+
+const FindCarForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FindCarForm
---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import HeroSlider from "../components/UI/HeroSlider";
+import Helmet from '../components/Helmet/Helmet';
+import { Container, Row, Col } from "reactstrap";
+
+const Home = () => {
+  return (
+    <Helmet title="Home">
+
+      {/* hero section */}
+      <section className="p-0 hero__slider-section">
+        <HeroSlider />
+
+        <div className="hero__form">
+          <Container>
+            <Row className="form__row">
+              <Col lg='4' md='4'>
+              <div className="find__cars-left">
+                <h2>Find your best car here</h2>
+              </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+
+      </section>
+    </Helmet>
+  );
+};
+
+export default Home;
